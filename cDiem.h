58e05898bbb3d72afb65df9c5c376f4475fdb5cc
Kazato-ce <pn226960@gmail.com>
2025-04-13 09:42:40 +0700

    Add files via upload

---
+++ cDiem.h
@@ -0,0 +1,20 @@
+#ifndef CDIEM_H
+#define CDIEM_H
+
+#include <iostream>
+#include <cmath> 
+using namespace std;
+
+class cDiem {
+private:
+    float x, y; // tọa độ của điểm
+public:
+    void Nhap(); // nhập tọa độ điểm
+    void Xuat(); // xuất tọa độ điểm
+    void TinhTuyen(float a, float b); // tính tuyến tính
+    void PhongTo(float k); // phóng to điểm theo tỉ lệ k
+    void ThuNho(float k); // thu nhỏ điểm theo tỉ lệ k
+    void Quay(float goc); // quay điểm quanh gốc tọa độ theo góc goc
+};
+
+#endif // CDIEM_H
